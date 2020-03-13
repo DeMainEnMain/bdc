cf0fc4ce6c088ffcfc3d47dc7fb8846a29a01d4f
Matthieu Haefele <matthieu@haefele.fr>
2020-03-13 17:57:14 +0100

    Refactoring install script

---
+++ install_native_loc.sh
@@ -0,0 +1,3 @@
+cd src/bdc/
+npm install
+./node_modules/webpack/bin/webpack.js --config webpack.config.js --progress --colors
---
+++ launch.sh
@@ -0,0 +1,2 @@
+source set_env.sh 
+python3 src.bdc/manage.py runserver
